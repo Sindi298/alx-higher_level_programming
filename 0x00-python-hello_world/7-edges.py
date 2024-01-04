af256285202448ae9e70df3024840bbcb26fdad6
Sindi298 <sindiswamsubo71@gmail.com>
2024-01-04 17:19:42 +0200

    edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-first3 = (word[:3])
-last2 = (word[-2:])
-middle = (word[1:8])
-print(f"First 3 letters: {first3}")
-print(f"Last 2 letters: {last2}")
-print(f"Middle word: {middle}")
+word_first_3 = (word[:3])
+word_first_2 = (word[-2:])
+middle_word = (word[1:8])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_first_2}")
+print(f"Middle word: {middle_word}")
