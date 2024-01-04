04e9bce7f8b0e403d497eb80cd0d0a676fad1e28
Sindi298 <sindiswamsubo71@gmail.com>
2024-01-04 17:14:37 +0200

    edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-first3 = "Hol"
-last2 = "on"
-middle = "olberto"
+first3 = (word[:3])
+last2 = (word[-2:])
+middle = (word[1:8])
 print(f"First 3 letters: {first3}")
 print(f"Last 2 letters: {last2}")
 print(f"Middle word: {middle}")
