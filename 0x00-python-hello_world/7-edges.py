aae6d6f83da0be263889568b6f9c820759299cbb
Sindi298 <sindiswamsubo71@gmail.com>
2024-01-04 17:09:32 +0200

    edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first3 = "Hol"
+last2 = "on"
+middle = "olberto"
+print(f"First 3 letters: {first3}")
+print(f"Last 2 letters: {last2}")
+print(f"Middle word: {middle}")
